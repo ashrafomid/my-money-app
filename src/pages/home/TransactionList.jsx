a8d133079828e5478f3f50efc9d51f536c555e7c
Mohammad Ashraf <ashrafomid75@gmail.com>
2023-02-12 16:50:21 +0430

    transactionList

--- src/pages/home/TransactionList.jsx
+++ src/pages/home/TransactionList.jsx
@@ -3,6 +3,18 @@ import React, { useState } from 'react'
 export default function TransactionList({transactions}) {
   const [data, setData] = useState('')
   return (
-    <div></div>
+    <div>
+      <form >
+        <label >
+          <span className=''>name</span>
+
+        </label>
+        <label >
+          <input type="text"
+          onChange={(e)=>setData(e.target.value)}
+          />
+        </label>
+      </form>
+    </div>
   )
 }
